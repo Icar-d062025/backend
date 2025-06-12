ac4be86f30729262821ee0fc42f9a6882063f495
RikiLaNeko <arthur.mortreux@lyceeastier.com>
2025-06-12 10:04:19 +0200

    initialize project with Spring Boot and Maven wrapper

---
+++ src/main/java/xyz/dedsecm/vroomvroomcar/VroomvroomcarApplication.java
@@ -0,0 +1,13 @@
+package xyz.dedsecm.vroomvroomcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VroomvroomcarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VroomvroomcarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/dedsecm/vroomvroomcar/VroomvroomcarApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.dedsecm.vroomvroomcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VroomvroomcarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
