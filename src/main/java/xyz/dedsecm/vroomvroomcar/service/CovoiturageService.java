809398b2603cf90b378cd31ce979708286b83a40
Fromont-L <lucasfromont97@gmail.com>
2025-06-13 11:15:43 +0200

    Ajouts : CovoiturageDTO, CovoiturageMapper, CovoiturageRepository, CovoiturageController. Modifications : CovoiturageService

--- src/main/java/xyz/dedsecm/vroomvroomcar/service/CovoiturageService.java
+++ src/main/java/xyz/dedsecm/vroomvroomcar/service/CovoiturageService.java
@@ -1,10 +1,21 @@
 package xyz.dedsecm.vroomvroomcar.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import xyz.dedsecm.vroomvroomcar.dto.CovoiturageDTO;
+import xyz.dedsecm.vroomvroomcar.mapper.CovoiturageMapper;
 import xyz.dedsecm.vroomvroomcar.model.Covoiturage;
+import xyz.dedsecm.vroomvroomcar.repository.CovoiturageRepository;
 
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
+@Service
 public class CovoiturageService {
+
+    @Autowired
+    private CovoiturageRepository covoiturageRepository;
+
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd/MM/yyyy : HH:mm");
 
@@ -14,5 +25,38 @@ public class CovoiturageService {
 
     public String formatDateHeureArrivee(Covoiturage covoiturage) {
         return covoiturage.getDateHeureArrivee().format(FORMATTER);
+    }
+
+    /***
+     * Permet de récupérer tous les covoiturages
+     * @return
+     */
+
+    public List<CovoiturageDTO> getAllCovoiturages() {
+        return covoiturageRepository.findAll().stream()
+                .map(CovoiturageMapper::toDTO).toList();
+    }
+
+    /***
+     * Permet d'ajouter un covoiturage
+     * @param covoiturageDTO
+     * @return
+     */
+
+    public CovoiturageDTO createCovoiturage(CovoiturageDTO covoiturageDTO) {
+        Covoiturage covoiturage = CovoiturageMapper.toEntity(covoiturageDTO);
+
+        covoiturage.setDateHeureDepart(covoiturageDTO.getDateHeureDepart());
+        covoiturage.setDateHeureArrivee(covoiturageDTO.getDateHeureArrivee());
+        covoiturage.setAdresseDepart(covoiturageDTO.getAdresseDepart());
+        covoiturage.setAdresseArrivee(covoiturageDTO.getAdresseArrivee());
+        covoiturage.setNbPlaces(covoiturageDTO.getNbPlaces());
+        covoiturage.setNbRestant(covoiturageDTO.getNbRestant());
+        covoiturage.setDistance(covoiturageDTO.getDistance());
+        covoiturage.setStatut(covoiturageDTO.getStatut());
+
+        Covoiturage savedCovoiturage = covoiturageRepository.save(covoiturage);
+
+        return CovoiturageMapper.toDTO(savedCovoiturage);
     }
 }
